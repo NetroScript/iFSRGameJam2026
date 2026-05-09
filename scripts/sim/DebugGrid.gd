7c8e30b36424c8edb4027113597d9b3efc76fa59
Paul Hempel <paul.hempel@tu-dresden.de>
2026-05-09 10:02:07 +0200

    Fix ant behavior by varying strength of dropped pheromones

--- scripts/sim/DebugGrid.gd
+++ scripts/sim/DebugGrid.gd
@@ -7,12 +7,18 @@ var ants: Array[Ant]
 
 func _ready():
 	world.init()
-	
-	var ant = Ant.new()
-	ant.pos = Vector2i(55, 35)
-	ant.heading = Vector2(1, 0)
-	ant.phero = World.Pheromone.Food
-	ants.push_back(ant)
+
+	for y in range(100):
+		world.put_food(world.nest.x + 20, y, 0)
+		world.put_food(world.nest.x - 20, y, 0)
+
+	ants.resize(20)
+	for i in range(ants.size()):
+		var ant = Ant.new()
+		ant.pos = world.nest
+		ant.heading = Vector2.ZERO
+		ant.goal = World.Pheromone.Food
+		ants[i] = ant
 
 func _draw():
 	if world.int_data.size() == 0: return
@@ -22,17 +28,25 @@ func _draw():
 		for y in range(world.size.y):
 			var color = Color(
 				world.phero_at(x, y, World.Pheromone.Food) / float(World.PHERO_MAX),
-				0,
+				0 if world.food_at(x, y) == World.NO_FOOD else 1,
 				world.phero_at(x, y, World.Pheromone.Home) / float(World.PHERO_MAX),
 			)
 			draw_rect(Rect2(10*x, 10*y, 10, 10), color)
+	for ant in ants:
+		draw_rect(
+			Rect2(10*ant.pos.x, 10*ant.pos.y, 10, 10),
+			Color(0.5, 0.5 ,0.5) if ant.food_id == World.NO_FOOD else Color.WHITE
+		)
 
+var redraw: int = 0
 func _process(_delta: float):
-	if Time.get_ticks_msec() / 5000 > ants.size():
-		var ant = Ant.new()
-		ant.pos = Vector2i(55, 35)
-		ant.phero = World.Pheromone.Food
-		ants.push_back(ant)
+	world.put_phero(world.nest.x, world.nest.y, World.Pheromone.Home, World.PHERO_MAX)
+
 	for i in range(ants.size()):
-		ants[i].step(world, i == 0)
+		ants[i].step(world, Vector2.ZERO)
+
+	if redraw < 5:
+		redraw += 1
+		return
+	redraw = 0
 	queue_redraw()
