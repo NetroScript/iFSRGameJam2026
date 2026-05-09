105b9284f4c96c18776167252eac2abf4db2c788
NetroScript <noreply@enostrion.com>
2026-05-09 12:22:49 +0200

    Change textures of food resource

--- scripts/food.gd
+++ scripts/food.gd
@@ -1,8 +1,10 @@
 class_name Food
 extends Resource
 
-## Icon of the food
-@export var icon: Texture2D
+## World texture of the food
+@export var world_texture: Texture2D
+## Particle texture of the food (carried by ants)
+@export var particle_texture: Texture2D
 ## Name of the food
 @export var name: String
 ## Calories of one unit of the food. More = better
